378fd40023a2da4517376f096f5f1ad2d09f7cf2
newu2012 <newu2011@gmail.com>
2021-12-09 18:59:44 +0500

    feat: change trips_list_page and add trip_dao

---
+++ lib/trip/data/trip.dart
@@ -0,0 +1,46 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Trip {
+  final String? creatorId;
+  final String title;
+  final int currentCompanions;
+  final int maximumCompanions;
+  final int costOverall;
+  int get oneUserCost => (costOverall / (currentCompanions + 1)).round();
+  final DateTime departureTime;
+
+  DocumentReference? reference;
+
+  Trip(
+      {required this.creatorId,
+      required this.title,
+      required this.currentCompanions,
+      required this.maximumCompanions,
+      required this.costOverall,
+      required this.departureTime,
+      this.reference});
+
+  factory Trip.fromJson(Map<dynamic, dynamic> json) => Trip(
+        creatorId: json['creatorId'] as String,
+        title: json['title'] as String,
+        currentCompanions: json['currentCompanions'] as int,
+        maximumCompanions: json['maximumCompanions'] as int,
+        costOverall: json['costOverall'] as int,
+        departureTime: (json['departureTime'] as Timestamp).toDate(),
+      );
+
+  Map<String, dynamic> toJson() => <String, dynamic>{
+        'creatorId': creatorId,
+        'title': title,
+        'currentCompanions': currentCompanions,
+        'maximumCompanions': maximumCompanions,
+        'costOverall': costOverall,
+        'departureTime': departureTime,
+      };
+
+  factory Trip.fromSnapshot(DocumentSnapshot snapshot) {
+    final trip = Trip.fromJson(snapshot.data() as Map<String, dynamic>);
+    trip.reference = snapshot.reference;
+    return trip;
+  }
+}
