172d5ff6ef1b4469d1c81d8f33fc13fb4d9ea7f3
newu2012 <newu2011@gmail.com>
2021-12-11 02:00:11 +0500

    feat: add basic toPoint and fromPoint trip fields

--- lib/trip/presentation/widgets/trip_list_tile.dart
+++ lib/trip/presentation/widgets/trip_list_tile.dart
@@ -1,6 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:provider/provider.dart';
 
+import '../../../common/data/fire_user_dao.dart';
+import '../../../common/data/user.dart';
+import '../../../common/data/user_dao.dart';
 import '../../data/trip.dart';
 
 class TripListTile extends StatelessWidget {
@@ -10,7 +17,10 @@ class TripListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final userDao = Provider.of<UserDao>(context, listen: false);
+
     return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -34,10 +44,80 @@ class TripListTile extends StatelessWidget {
               const Icon(Icons.schedule),
               Text(DateFormat('HH:mm').format(trip.departureTime)),
             ],
-          )
-        ],
-      ),
-      padding: const EdgeInsets.symmetric(horizontal: 24),
+          ),
+              FutureBuilder(
+                future: userDao.getUserByUid(FireUserDao().userId()),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    final user = snapshot.data as User;
+                    return Row(
+                      children: [
+                        DistanceFromPoint(user: user, point: trip.fromPoint),
+                        DistanceToPoint(user: user, point: trip.toPoint),
+                      ],
+                    );
+                  } else {
+                    return const LinearProgressIndicator();
+                  }
+                },
+              ),
+            ],
+          ),
+    );
+  }
+}
+
+double _calculateMeterDistance(LatLng p1, LatLng p2) {
+  const distance = Distance();
+  return distance(p1, p2);
+}
+
+class DistanceFromPoint extends StatelessWidget {
+  final GeoPoint point;
+  final User user;
+
+  const DistanceFromPoint({Key? key, required this.user, required this.point})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final meterDistance = _calculateMeterDistance(
+        LatLng(user.fromPoint!.latitude, user.fromPoint!.longitude),
+        LatLng(point.latitude, point.longitude));
+
+    return Row(
+      children: [
+        const Icon(MdiIcons.carArrowLeft),
+        SizedBox(
+          width: 50,
+          child: Text('${meterDistance}'),
+        ),
+      ],
+    );
+  }
+}
+
+class DistanceToPoint extends StatelessWidget {
+  final GeoPoint point;
+  final User user;
+
+  const DistanceToPoint({Key? key, required this.user, required this.point})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final meterDistance = _calculateMeterDistance(
+        LatLng(user.toPoint!.latitude, user.toPoint!.longitude),
+        LatLng(point.latitude, point.longitude));
+
+    return Row(
+      children: [
+        const Icon(MdiIcons.carArrowRight),
+        SizedBox(
+          width: 50,
+          child: Text('${meterDistance}'),
+        ),
+      ],
     );
   }
 }
