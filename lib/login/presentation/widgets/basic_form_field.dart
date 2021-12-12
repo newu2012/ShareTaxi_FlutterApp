a0234940072685bc06a88aa37756967411f72831
newu2012 <newu2011@gmail.com>
2021-12-12 17:45:02 +0500

    refactor: refactor login_page and signup_page

---
+++ lib/login/presentation/widgets/basic_form_field.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import '../../logic/validators.dart';
+
+class BasicFormField extends StatelessWidget {
+  const BasicFormField({
+    Key? key,
+    required TextEditingController controller,
+    required String? hint,
+    required String ifEmptyOrNull,
+  })  : _controller = controller,
+        _hint = hint,
+        _ifEmptyOrNull = ifEmptyOrNull,
+        super(key: key);
+
+  final TextEditingController _controller;
+  final String? _hint;
+  final String _ifEmptyOrNull;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        hintText: _hint,
+      ),
+      controller: _controller,
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      validator: (value) => Validators.basicValidator(value, _ifEmptyOrNull),
+    );
+  }
+}
---
+++ lib/login/presentation/widgets/password_confirm_form_field.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+import '../../logic/validators.dart';
+
+class PasswordConfirmFormField extends StatefulWidget {
+  const PasswordConfirmFormField({
+    Key? key,
+    required TextEditingController passwordController,
+    required TextEditingController passwordConfirmController,
+  })  : _passwordController = passwordController,
+        _passwordConfirmController = passwordConfirmController,
+        super(key: key);
+
+  final TextEditingController _passwordController;
+  final TextEditingController _passwordConfirmController;
+
+  @override
+  State<PasswordConfirmFormField> createState() =>
+      _PasswordConfirmFormFieldState();
+}
+
+class _PasswordConfirmFormFieldState extends State<PasswordConfirmFormField> {
+  bool _obfuscatePasswordConfirm = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: widget._passwordConfirmController,
+      obscureText: _obfuscatePasswordConfirm,
+      decoration: InputDecoration(
+        hintText: 'Повторите пароль',
+        suffixIcon: IconButton(
+          onPressed: () {
+            setState(
+              () => _obfuscatePasswordConfirm = !_obfuscatePasswordConfirm,
+            );
+          },
+          icon: Icon(
+            _obfuscatePasswordConfirm ? Icons.visibility : Icons.visibility_off,
+            color: Theme.of(context).primaryColor,
+          ),
+        ),
+      ),
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      validator: (value) => Validators.confirmPasswordValidator(
+        value,
+        widget._passwordController,
+      ),
+    );
+  }
+}
