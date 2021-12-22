1d10e563bd242528b3c3a4f693dcf4ba61d5c3d4
newu2012 <newu2011@gmail.com>
2021-12-22 20:17:55 +0500

    feat: CreateTripPage add AppBar, refactor TripForm

--- lib/trip/presentation/pages/create_trip_page.dart
+++ lib/trip/presentation/pages/create_trip_page.dart
@@ -13,16 +13,15 @@ class _CreateTripPageState extends State<CreateTripPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Создание поездки'),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 111, 108, 217),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            const Text(
-              'Создание поездки',
-              style: TextStyle(
-                fontSize: 36,
-              ),
-            ),
             Expanded(
               child: ListView(
                 children: [
