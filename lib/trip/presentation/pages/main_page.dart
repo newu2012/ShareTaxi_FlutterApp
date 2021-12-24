e48814391cbd05c64ddc737f27db6a83b5218307
newu2012 <newu2011@gmail.com>
2021-12-24 09:14:46 +0500

    feat: РАБОЧИЙ ПОИСК АДРЕСА, КАК СЛЫШНО, ПРИЁМ

--- lib/trip/presentation/pages/main_page.dart
+++ lib/trip/presentation/pages/main_page.dart
@@ -1,11 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  late GoogleMapController mapController;
+  late String searchAddress;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          GoogleMap(
+            onMapCreated: onMapCreated,
+            initialCameraPosition: const CameraPosition(
+              target: LatLng(40.7128, -74.006),
+              zoom: 10.0,
+            ),
+          ),
+          Positioned(
+            top: 30.0,
+            right: 15.0,
+            left: 15.0,
+            child: Container(
+              height: 50.0,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                color: Colors.white,
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Enter Address',
+                  border: InputBorder.none,
+                  contentPadding: const EdgeInsets.only(left: 15.0, top: 15.0),
+                  suffixIcon: IconButton(
+                    icon: const Icon(Icons.search),
+                    onPressed: searchAndNavigate,
+                    iconSize: 30.0,
+                  ),
+                ),
+                onChanged: (val) {
+                  setState(() {
+                    searchAddress = val;
+                  });
+                },
+              ),
+            ),
+          ),
+          //
+          // TODO BottomSheet
+        ],
+      ),
     );
+  }
+
+  void searchAndNavigate() async {
+    final location =
+        await GeocodingPlatform.instance.locationFromAddress(searchAddress);
+
+    print(location.first);
+
+    GeocodingPlatform.instance
+        .placemarkFromCoordinates(
+      location.first.latitude,
+      location.first.longitude,
+    )
+        .then((result) {
+      mapController.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+        target: LatLng(location.first.latitude, location.first.longitude),
+        zoom: 15.0,
+      )));
+    });
+  }
+
+  void onMapCreated(controller) {
+    setState(() {
+      mapController = controller;
+    });
   }
 }
