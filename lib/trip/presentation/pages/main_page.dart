864ba207da0f866ee927292494680ede84e0f689
newu2012 <newu2011@gmail.com>
2021-12-24 11:02:25 +0500

    feat: add two search bars, center between markers

--- lib/trip/presentation/pages/main_page.dart
+++ lib/trip/presentation/pages/main_page.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MainPage extends StatefulWidget {
@@ -11,7 +14,17 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   late GoogleMapController mapController;
-  late String searchAddress;
+  late String fromPointAddress;
+  late String toPointAddress;
+  Marker? fromPointMarker;
+  Marker? toPointMarker;
+  Set<Marker> get _markers {
+    final markers = <Marker>{};
+    if (fromPointMarker != null) markers.add(fromPointMarker!);
+    if (toPointMarker != null) markers.add(toPointMarker!);
+
+    return markers;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,39 +32,73 @@ class _MainPageState extends State<MainPage> {
       body: Stack(
         children: <Widget>[
           GoogleMap(
-            onMapCreated: onMapCreated,
+            onMapCreated: _onMapCreated,
             initialCameraPosition: const CameraPosition(
-              target: LatLng(40.7128, -74.006),
+              target: LatLng(56.843, 69.645),
               zoom: 10.0,
             ),
+            markers: _markers,
           ),
           Positioned(
             top: 30.0,
             right: 15.0,
             left: 15.0,
             child: Container(
-              height: 50.0,
               width: double.infinity,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(10.0),
                 color: Colors.white,
               ),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'Enter Address',
-                  border: InputBorder.none,
-                  contentPadding: const EdgeInsets.only(left: 15.0, top: 15.0),
-                  suffixIcon: IconButton(
-                    icon: const Icon(Icons.search),
-                    onPressed: searchAndNavigate,
-                    iconSize: 30.0,
+              child: Column(
+                children: [
+                  TextField(
+                    onSubmitted: (value) => searchAndNavigate,
+                    onEditingComplete: () =>
+                        searchAndNavigate(fromPointAddress, 'fromPoint'),
+                    textInputAction: TextInputAction.search,
+                    decoration: InputDecoration(
+                      hintText: 'Откуда поедем',
+                      border: InputBorder.none,
+                      contentPadding:
+                          const EdgeInsets.only(left: 15.0, top: 15.0),
+                      suffixIcon: IconButton(
+                        icon: const Icon(Icons.search),
+                        onPressed: () =>
+                            searchAndNavigate(fromPointAddress, 'fromPoint'),
+                        iconSize: 30.0,
+                      ),
+                    ),
+                    onChanged: (val) {
+                      setState(() {
+                        fromPointAddress = val;
+                      });
+                    },
                   ),
-                ),
-                onChanged: (val) {
-                  setState(() {
-                    searchAddress = val;
-                  });
-                },
+                  const Divider(),
+                  TextField(
+                    onSubmitted: (value) => searchAndNavigate,
+                    onEditingComplete: () =>
+                        searchAndNavigate(toPointAddress, 'toPoint'),
+                    textInputAction: TextInputAction.search,
+                    decoration: InputDecoration(
+                      hintText: 'Куда поедем',
+                      border: InputBorder.none,
+                      contentPadding:
+                          const EdgeInsets.only(left: 15.0, top: 15.0),
+                      suffixIcon: IconButton(
+                        icon: const Icon(Icons.search),
+                        onPressed: () =>
+                            searchAndNavigate(toPointAddress, 'toPoint'),
+                        iconSize: 30.0,
+                      ),
+                    ),
+                    onChanged: (val) {
+                      setState(() {
+                        toPointAddress = val;
+                      });
+                    },
+                  ),
+                ],
               ),
             ),
           ),
@@ -62,28 +109,66 @@ class _MainPageState extends State<MainPage> {
     );
   }
 
-  void searchAndNavigate() async {
-    final location =
-        await GeocodingPlatform.instance.locationFromAddress(searchAddress);
+  void searchAndNavigate(String address, String pointName) async {
+    final locations =
+        (await GeocodingPlatform.instance.locationFromAddress(address))
+            .map((e) => LatLng(e.latitude, e.longitude));
 
-    print(location.first);
-
-    GeocodingPlatform.instance
-        .placemarkFromCoordinates(
-      location.first.latitude,
-      location.first.longitude,
-    )
-        .then((result) {
-      mapController.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
-        target: LatLng(location.first.latitude, location.first.longitude),
-        zoom: 15.0,
-      )));
+    print(locations.first);
+    setState(() {
+      if (pointName == 'fromPoint')
+        fromPointMarker = _createMarker(locations.first, pointName);
+      else if (pointName == 'toPoint')
+        toPointMarker = _createMarker(locations.first, pointName);
     });
+
+    _moveCamera();
   }
 
-  void onMapCreated(controller) {
+  void _moveCamera() {
+    LatLng target;
+    target = _markers.length == 2
+        ? LatLng(
+            (_markers.first.position.latitude +
+                    _markers.last.position.latitude) /
+                2,
+            (_markers.first.position.longitude +
+                    _markers.last.position.longitude) /
+                2,
+          )
+        : _markers.first.position;
+    print(target);
+
+    var zoomLevel = 12.0;
+    if (_markers.length == 2) {
+      final radius = GeolocatorPlatform.instance.distanceBetween(
+        _markers.first.position.latitude,
+        _markers.first.position.longitude,
+        _markers.last.position.latitude,
+        _markers.last.position.longitude,
+      );
+      final scale = radius / 500;
+      zoomLevel = (16 - log(scale * 1.2) / log(2));
+    }
+    print(zoomLevel);
+
+    mapController.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+      target: target,
+      zoom: zoomLevel,
+    )));
+  }
+
+  void _onMapCreated(controller) {
     setState(() {
       mapController = controller;
     });
+  }
+
+  Marker _createMarker(LatLng position, String pointName) {
+    return Marker(
+      markerId: MarkerId(pointName),
+      position: position,
+      icon: BitmapDescriptor.defaultMarker,
+    );
   }
 }
