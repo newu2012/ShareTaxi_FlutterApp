ddda3ba8876a090554ffd74836df5fe23049f91b
newu2012 <newu2011@gmail.com>
2021-12-27 17:35:36 +0500

    fix: fix random code

--- lib/trip/presentation/pages/main_page.dart
+++ lib/trip/presentation/pages/main_page.dart
@@ -1,6 +1,5 @@
 import 'dart:math';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
@@ -53,7 +52,6 @@ class _MainPageState extends State<MainPage> {
               child: Column(
                 children: [
                   TextField(
-                    focusNode: focus,
                     onSubmitted: (value) => searchAndNavigate,
                     onEditingComplete: () =>
                         searchAndNavigate(fromPointAddress, 'fromPoint'),
