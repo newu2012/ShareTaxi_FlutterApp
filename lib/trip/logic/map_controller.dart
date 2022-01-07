590aa48db277e889cbe5a56faf063691458e7616
newu2012 <newu2011@gmail.com>
2022-01-07 13:50:38 +0500

    feat: add style on main page and createTripPage

--- lib/trip/logic/map_controller.dart
+++ lib/trip/logic/map_controller.dart
@@ -4,4 +4,7 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 class MapController extends ChangeNotifier {
   late GoogleMapController mapController;
   Set<Marker> markers = {};
+
+  late String fromPointAddress;
+  late String toPointAddress;
 }
