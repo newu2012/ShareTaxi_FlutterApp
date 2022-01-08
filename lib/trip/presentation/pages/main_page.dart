df953f217d2891f91b1a78ab630a7bf245ac5465
newu2012 <newu2011@gmail.com>
2022-01-09 04:47:22 +0500

    feat: add address Icon color on MainPage

--- lib/trip/presentation/pages/main_page.dart
+++ lib/trip/presentation/pages/main_page.dart
@@ -106,16 +106,20 @@ class _MainPageState extends State<MainPage> {
                             padding: const EdgeInsets.all(8.0),
                             child: Row(
                               children: [
-                                const Icon(Icons.location_on),
+                                Icon(
+                                  Icons.location_on,
+                                  color: Theme.of(context).primaryColor,
+                                ),
                                 const Text('От'),
                               ],
                             ),
                           ),
                         ),
-                        prefixIconColor:
-                            const Color.fromARGB(255, 111, 108, 217),
                         suffixIcon: IconButton(
-                          icon: const Icon(Icons.search),
+                          icon: Icon(
+                            Icons.search,
+                            color: Theme.of(context).primaryColor,
+                          ),
                           onPressed: () =>
                               searchAndNavigate(fromPointAddress, 'fromPoint'),
                           iconSize: 30.0,
@@ -146,15 +150,20 @@ class _MainPageState extends State<MainPage> {
                             padding: const EdgeInsets.all(8.0),
                             child: Row(
                               children: [
-                                const Icon(Icons.location_on),
+                                const Icon(
+                                  Icons.location_on,
+                                  color: Color.fromRGBO(255, 174, 3, 100),
+                                ),
                                 const Text('До'),
                               ],
                             ),
                           ),
                         ),
-                        prefixIconColor: const Color.fromARGB(255, 255, 174, 3),
                         suffixIcon: IconButton(
-                          icon: const Icon(Icons.search),
+                          icon: Icon(
+                            Icons.search,
+                            color: Theme.of(context).primaryColor,
+                          ),
                           onPressed: () =>
                               searchAndNavigate(toPointAddress, 'toPoint'),
                           iconSize: 30.0,
