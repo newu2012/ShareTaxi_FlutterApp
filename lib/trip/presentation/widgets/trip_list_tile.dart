944be5b7d49ac5c3bf81be498a11e6b9528f21e6
newu2012 <newu2011@gmail.com>
2022-01-11 01:47:56 +0500

    feat: refactor TripListTile, add text variations

--- lib/trip/presentation/widgets/trip_list_tile.dart
+++ lib/trip/presentation/widgets/trip_list_tile.dart
@@ -9,7 +9,7 @@ import '../../logic/map_controller.dart';
 import '../../data/trip.dart';
 
 class TripListTile extends StatelessWidget {
-  const TripListTile(Trip this.trip, {Key? key}) : super(key: key);
+  const TripListTile(this.trip, {Key? key}) : super(key: key);
 
   final Trip trip;
 
@@ -30,45 +30,164 @@ class TripListTile extends StatelessWidget {
               style: const TextStyle(fontSize: 19),
             ),
             DistanceAndAddresses(trip: trip),
-            Row(
-              children: [
-                Icon(
-                  Icons.schedule,
-                  color: Theme.of(context).primaryColor,
-                ),
-                const SizedBox(
-                  width: 4,
-                ),
-                Text('${DateFormat('HH:mm').format(trip.departureTime)} выезд'),
-                const SizedBox(
-                  width: 12,
-                ),
-                Icon(
-                  Icons.people,
-                  color: Theme.of(context).primaryColor,
-                ),
-                const SizedBox(
-                  width: 4,
-                ),
-                Text(
-                  '${trip.currentCompanions.length}/${trip.maximumCompanions}',
-                ),
-                const SizedBox(
-                  width: 12,
-                ),
-                Icon(
-                  Icons.payments,
-                  color: Theme.of(context).primaryColor,
-                ),
-                const SizedBox(
-                  width: 4,
-                ),
-                Text('${trip.costOverall}/${trip.oneUserCost} руб.'),
-              ],
-            ),
+            TripMainInfoRow(trip: trip),
           ],
         ),
       ),
+    );
+  }
+}
+
+class TripMainInfoRow extends StatelessWidget {
+  const TripMainInfoRow({
+    Key? key,
+    required this.trip,
+  }) : super(key: key);
+
+  final Trip trip;
+
+  @override
+  Widget build(BuildContext context) {
+    final costOverall = trip.costOverall >= 1000
+        ? '${(trip.costOverall / 1000).toStringAsFixed(1)} к'
+        : '${trip.costOverall}';
+    final costOneUser = trip.oneUserCost >= 1000
+        ? '${(trip.oneUserCost / 1000).toStringAsFixed(1)} к'
+        : '${trip.oneUserCost}';
+    final costText = costOverall.endsWith('к') && costOneUser.endsWith('к')
+        ? Text('$costOverall/$costOneUser')
+        : Text('$costOverall/$costOneUser руб.');
+
+    return Row(
+      children: [
+        Icon(
+          Icons.schedule,
+          color: Theme.of(context).primaryColor,
+        ),
+        const SizedBox(
+          width: 4,
+        ),
+        Text('${DateFormat('HH:mm').format(trip.departureTime)} выезд'),
+        const SizedBox(
+          width: 12,
+        ),
+        Icon(
+          Icons.people,
+          color: Theme.of(context).primaryColor,
+        ),
+        const SizedBox(
+          width: 4,
+        ),
+        Text(
+          '${trip.currentCompanions.length}/${trip.maximumCompanions}',
+        ),
+        const SizedBox(
+          width: 12,
+        ),
+        Icon(
+          Icons.payments,
+          color: Theme.of(context).primaryColor,
+        ),
+        const SizedBox(
+          width: 4,
+        ),
+        costText,
+      ],
+    );
+  }
+}
+
+class DistanceAndAddresses extends StatelessWidget {
+  const DistanceAndAddresses({
+    Key? key,
+    required this.trip,
+  }) : super(key: key);
+
+  final Trip trip;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            DistanceBetweenPoints(
+              fromPoint: Provider.of<MapController>(context, listen: false)
+                  .fromPointAddress,
+              toPoint: trip.fromPointAddress,
+              fromUser: true,
+            ),
+            DistanceBetweenPoints(
+              fromPoint: Provider.of<MapController>(context, listen: false)
+                  .toPointAddress,
+              toPoint: trip.toPointAddress,
+              fromUser: false,
+            ),
+          ],
+        ),
+        const SizedBox(
+          width: 8,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(trip.fromPointAddress),
+            const SizedBox(
+              height: 8,
+            ),
+            Text(trip.toPointAddress),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class DistanceBetweenPoints extends StatelessWidget {
+  final String fromPoint;
+  final String toPoint;
+  final bool fromUser;
+
+  const DistanceBetweenPoints({
+    Key? key,
+    required this.fromPoint,
+    required this.toPoint,
+    required this.fromUser,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final icon = fromUser
+        ? Icon(
+            MdiIcons.carArrowLeft,
+            color: Theme.of(context).primaryColor,
+          )
+        : const Icon(
+            MdiIcons.carArrowRight,
+            color: Color.fromRGBO(255, 174, 3, 100),
+          );
+
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        icon,
+        const SizedBox(
+          width: 4,
+        ),
+        FutureBuilder(
+          future: getDistance(fromPoint, toPoint),
+          builder: (_, snapshot) {
+            if (!snapshot.hasData) return const Text('...');
+            final distance = snapshot.data as int;
+
+            return distance > 1000
+                ? Text('${(distance / 1000).toStringAsFixed(1)} км.')
+                : Text('${distance} м.');
+          },
+        ),
+      ],
     );
   }
 }
@@ -92,115 +211,4 @@ double _calculateMeterDistance(LatLng p1, LatLng p2) {
   const distance = Distance();
 
   return distance(p1, p2);
-}
-
-class DistanceAndAddresses extends StatelessWidget {
-  const DistanceAndAddresses({
-    Key? key,
-    required this.trip,
-  }) : super(key: key);
-
-  final Trip trip;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          children: [
-            DistanceFromPoint(
-              fromPoint: Provider.of<MapController>(context, listen: false)
-                  .fromPointAddress,
-              toPoint: trip.fromPointAddress,
-            ),
-            Text(trip.fromPointAddress),
-          ],
-        ),
-        Row(
-          children: [
-            DistanceToPoint(
-              fromPoint: Provider.of<MapController>(context, listen: false)
-                  .toPointAddress,
-              toPoint: trip.toPointAddress,
-            ),
-            Text(trip.toPointAddress),
-          ],
-        ),
-      ],
-    );
-  }
-}
-
-//  TODO Сделать общий класс вместо Distance...
-class DistanceFromPoint extends StatelessWidget {
-  final String fromPoint;
-  final String toPoint;
-
-  const DistanceFromPoint({
-    Key? key,
-    required this.fromPoint,
-    required this.toPoint,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Icon(
-          MdiIcons.carArrowLeft,
-          color: Theme.of(context).primaryColor,
-        ),
-        const SizedBox(
-          width: 4,
-        ),
-        SizedBox(
-          width: 60,
-          child: FutureBuilder(
-            future: getDistance(fromPoint, toPoint),
-            builder: (_, snapshot) => Text('${snapshot.data} м.'),
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class DistanceToPoint extends StatelessWidget {
-  final String fromPoint;
-  final String toPoint;
-
-  const DistanceToPoint({
-    Key? key,
-    required this.fromPoint,
-    required this.toPoint,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        const Icon(
-          MdiIcons.carArrowRight,
-          color: Color.fromRGBO(255, 174, 3, 100),
-        ),
-        const SizedBox(
-          width: 4,
-        ),
-        SizedBox(
-          width: 60,
-          child: FutureBuilder(
-            future: getDistance(fromPoint, toPoint),
-            builder: (_, snapshot) {
-              final distance = snapshot.data as int;
-
-              return distance > 1000 ?
-              Text('${(distance / 1000).toStringAsFixed(1)} км.') :
-              Text('${distance} м.');
-
-            },
-          ),
-        ),
-      ],
-    );
-  }
 }
