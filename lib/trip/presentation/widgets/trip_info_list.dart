ec2553b35885412c18c326b57642c763855e2e70
newu2012 <newu2011@gmail.com>
2022-01-18 05:41:15 +0500

    feat: add TripInfoPage Cards and Companions rows

--- lib/trip/presentation/widgets/trip_info_list.dart
+++ lib/trip/presentation/widgets/trip_info_list.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:share_taxi/common/data/user.dart';
+import 'package:share_taxi/common/data/user_dao.dart';
 
 import '../../../chat/presentation/widgets/chat_list_tile.dart';
 import 'distance_and_addresses_row.dart';
@@ -32,31 +34,45 @@ class _TripInfoListState extends State<TripInfoList> {
         if (!snapshot.hasData) return const CircularProgressIndicator();
         _trip = Trip.fromSnapshot(snapshot.data!);
 
-        return Column(
-          children: [
-            const SizedBox(
-              height: 8,
-            ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Text(
-                _trip.title,
-                style: const TextStyle(fontSize: 19),
-                overflow: TextOverflow.fade,
+        return SizedBox(
+          width: 300,
+          height: 500,
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 8,
               ),
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            AddressRow(
-              trip: _trip,
-              withDistance: _trip.creatorId != _fireUserDao.userId(),
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            _buildMainInfoButton(),
-          ],
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Text(
+                  _trip.title,
+                  style: const TextStyle(fontSize: 19),
+                  overflow: TextOverflow.fade,
+                ),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              AddressRow(
+                trip: _trip,
+                withDistance: _trip.creatorId != _fireUserDao.userId(),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              TripInfoRow(trip: _trip),
+              const SizedBox(
+                height: 8,
+              ),
+              CompanionsRow(
+                trip: _trip,
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              _buildMainInfoButton(),
+            ],
+          ),
         );
       },
     );
@@ -79,6 +95,244 @@ class _TripInfoListState extends State<TripInfoList> {
       tripDao: _tripDao,
       trip: _trip,
       fireUserDao: _fireUserDao,
+    );
+  }
+}
+
+class TripInfoRow extends StatelessWidget {
+  const TripInfoRow({Key? key, required this.trip}) : super(key: key);
+  final Trip trip;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        TimeCard(
+          trip: trip,
+        ),
+        CompanionsCard(
+          trip: trip,
+        ),
+        CostCard(
+          trip: trip,
+        ),
+      ],
+    );
+  }
+}
+
+class TimeCard extends StatelessWidget {
+  const TimeCard({Key? key, required this.trip}) : super(key: key);
+  final Trip trip;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Card(
+        child: SizedBox(
+          height: 60,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.schedule,
+                color: Theme.of(context).primaryColor,
+              ),
+              const SizedBox(
+                width: 12,
+              ),
+              Text(
+                '${trip.departureTime.hour}:${trip.departureTime.minute}',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CompanionsCard extends StatelessWidget {
+  const CompanionsCard({Key? key, required this.trip}) : super(key: key);
+  final Trip trip;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Card(
+        child: SizedBox(
+          height: 60,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.people,
+                color: Theme.of(context).primaryColor,
+              ),
+              const SizedBox(
+                width: 12,
+              ),
+              Text(
+                '${trip.currentCompanions.length}/${trip.maximumCompanions}',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CostCard extends StatelessWidget {
+  const CostCard({Key? key, required this.trip}) : super(key: key);
+  final Trip trip;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Card(
+        child: SizedBox(
+          height: 60,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.payments,
+                color: Theme.of(context).primaryColor,
+              ),
+              const SizedBox(
+                width: 12,
+              ),
+              Text(
+                '${trip.oneUserCost}/\n ${trip.costOverall}',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CompanionsRow extends StatefulWidget {
+  const CompanionsRow({Key? key, required this.trip}) : super(key: key);
+  final Trip trip;
+
+  @override
+  _CompanionsRowState createState() => _CompanionsRowState();
+}
+
+class _CompanionsRowState extends State<CompanionsRow> {
+  @override
+  Widget build(BuildContext context) {
+    final _userDao = Provider.of<UserDao>(context, listen: false);
+
+    return Column(
+      children: [
+        const Text(
+          'Попутчики',
+          style: TextStyle(fontSize: 19.0),
+        ),
+        SizedBox(
+          height: 60,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: widget.trip.currentCompanions.length,
+            itemBuilder: (context, i) {
+              return FutureBuilder<User>(
+                future: _userDao.getUserByUid(
+                  widget.trip.currentCompanions[i],
+                ),
+                builder: (context, snapshot) => snapshot.hasData
+                    ? CompanionCard(companion: snapshot.data!)
+                    : const Center(child: LinearProgressIndicator()),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class CompanionCard extends StatelessWidget {
+  const CompanionCard({Key? key, required this.companion}) : super(key: key);
+  final User companion;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Container(
+        height: 60,
+        constraints: const BoxConstraints(maxWidth: 150),
+        padding: const EdgeInsets.all(8),
+        child: Row(
+          children: [
+            _buildMessageCreatorAvatar(companion.photoUrl),
+            const SizedBox(
+              width: 8,
+            ),
+            Expanded(
+              child: Text(
+                '${companion.firstName} ${companion.lastName}',
+                softWrap: true,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildMessageCreatorAvatar(String? userAvatarUrl) {
+    return CircleAvatar(
+      maxRadius: 16,
+      backgroundImage: NetworkImage(userAvatarUrl ??
+          'https://firebasestorage.googleapis.com/v0/b/newu-share-taxi.appspot.com/o/avatar.png?alt=media&token=735f2da2-a631-4650-9372-0ba9bfa673aa'),
+    );
+  }
+}
+
+class LeaveTripButton extends StatelessWidget {
+  const LeaveTripButton({
+    Key? key,
+    required this.trip,
+    required this.fireUserDao,
+    required this.tripDao,
+  }) : super(key: key);
+
+  final Trip trip;
+  final FireUserDao fireUserDao;
+  final TripDao tripDao;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        var newCompanions = List<String>.from(trip.currentCompanions);
+        newCompanions.remove(fireUserDao.userId()!);
+        newCompanions = newCompanions.toSet().toList();
+        final newTrip = Trip.fromTrip(
+          trip: trip,
+          currentCompanions: newCompanions,
+        );
+
+        tripDao.updateTrip(id: trip.reference!.id, trip: newTrip);
+        Navigator.pop(context);
+        Navigator.pop(context);
+      },
+      child: const Text(
+        'Покинуть поездку',
+      ),
+      style: ElevatedButton.styleFrom(
+        primary: const Color.fromARGB(255, 216, 57, 76),
+      ),
     );
   }
 }
@@ -130,44 +384,6 @@ class JoinTripButton extends StatelessWidget {
       },
       child: const Text(
         'Присоединиться к поездке',
-      ),
-    );
-  }
-}
-
-class LeaveTripButton extends StatelessWidget {
-  const LeaveTripButton({
-    Key? key,
-    required this.trip,
-    required this.fireUserDao,
-    required this.tripDao,
-  }) : super(key: key);
-
-  final Trip trip;
-  final FireUserDao fireUserDao;
-  final TripDao tripDao;
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        var newCompanions = List<String>.from(trip.currentCompanions);
-        newCompanions.remove(fireUserDao.userId()!);
-        newCompanions = newCompanions.toSet().toList();
-        final newTrip = Trip.fromTrip(
-          trip: trip,
-          currentCompanions: newCompanions,
-        );
-
-        tripDao.updateTrip(id: trip.reference!.id, trip: newTrip);
-        Navigator.pop(context);
-        Navigator.pop(context);
-      },
-      child: const Text(
-        'Покинуть поездку',
-      ),
-      style: ElevatedButton.styleFrom(
-        primary: const Color.fromARGB(255, 216, 57, 76),
       ),
     );
   }
