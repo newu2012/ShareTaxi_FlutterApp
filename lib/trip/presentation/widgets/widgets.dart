cf35a057174d58de5a88ddedd4a84bfbcc3ff3ac
newu2012 <newu2011@gmail.com>
2022-05-10 00:32:12 +0500

    add text scroll for long addresses in trips tiles

--- lib/trip/presentation/widgets/widgets.dart
+++ lib/trip/presentation/widgets/widgets.dart
@@ -5,5 +5,5 @@ export 'create_trip_form.dart';
 export 'trip_info_list.dart';
 export 'user_addresses_column.dart';
 export 'trip_main_info_row.dart';
-export 'distance_and_addresses_row.dart';
+export 'distance_and_addresses_column.dart';
 export 'distance_between_points_row.dart';
